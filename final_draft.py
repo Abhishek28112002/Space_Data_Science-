797cb5f584c1561614747a88abd63486e5bf2ba6
azrael18102001 <105088428+azrael18102001@users.noreply.github.com>
2022-06-12 12:44:52 +0530

    main

--- final_draft.py
+++ final_draft.py
@@ -145,6 +145,7 @@ with open(output_path, 'w', newline='') as write_obj:
             csv_writer.writerow([actual_Class[i]])
             i+=1
 print("succesfully written")
+print("features used for classification",cols)
 r=requests.get("http://./uploads/python.csv", stream=True)
 with open("output.csv", 'wb') as f:
     for chunk in r.iter_content(chunk_size=1024): 
