44b5c9c25ec011d98991330381b1b65aa7eff952
azrael18102001 <105088428+azrael18102001@users.noreply.github.com>
2022-06-12 11:14:35 +0530

    d

--- final_draft.py
+++ final_draft.py
@@ -52,7 +52,7 @@ def accu(pipe1 ,pipe3,df,dfr):
     sb.heatmap(cf,annot=True,xticklabels=['AFP','PC','NTP','UNK'],yticklabels=['AFP','PC','NTP','UNK'])
 
 #paths
-training_path=r"C:\Users\Asus\Documents\full_data.csv"  
+training_path=r"full_data.csv"  
 testing=r"./uploads/python.csv"
 output_path=r"./uploads/python.csv"
 cols=['tce_period', 'tce_time0bk_err', 'tce_impact_err', 'tce_depth', 'tce_depth_err', 'tce_prad_err', 'tce_steff_err', 'tce_slogg_err']
