7abb923ee44653f6195f0ef77769cedae82f60dd
azrael18102001 <105088428+azrael18102001@users.noreply.github.com>
2022-06-12 13:26:59 +0530

    main

--- final_draft.py
+++ final_draft.py
@@ -27,6 +27,8 @@ import seaborn as sb
 import os
 from csv import writer
 from csv import reader
+
+#print accuracy while testing on test data 
 def accu(pipe1 ,pipe3,df,dfr):
     X_test_accu=df.iloc[14000:15000,:].values
     y_test_accu=dfr.iloc[14000:15000,-1].values
@@ -71,7 +73,7 @@ testing=r"./uploads/python.csv"
 output_path=r"./uploads/python.csv"
 cols=['tce_period', 'tce_time0bk_err', 'tce_impact_err', 'tce_depth', 'tce_depth_err', 'tce_prad_err', 'tce_steff_err', 'tce_slogg_err']
 
-
+#creating testing and training datasets
 df=pd.read_csv(training_path,usecols=cols)
 dfr=pd.read_csv(training_path,usecols=['av_training_set'])
 X_train=df.iloc[:14000,:].values
@@ -95,27 +97,28 @@ os.remove(testing)
 pipe1 = Pipeline([("Standard Scaling",RobustScaler()),("SGD Regression",KNeighborsClassifier())])
 pipe1.fit(X_train, y_train)  # apply scaling on training data
 # print("K Nearest Neighbors Trained")
-# print("Mean absolute error",mean_absolute_error(Y_pred,y_test))
-proba_SVC=pipe1.predict_proba(X_test)
+
+proba_SVC=pipe1.predict_proba(X_test)               # predict on test data using trained model
 
 
 pipe3 = Pipeline([("Standard Scaling",RobustScaler()),("SGD Regression",RandomForestClassifier(random_state=12,n_estimators=100))])
 pipe3.fit(X_train, y_train) # apply scaling on training data
 # print("Random Forest Trained")
-# print("Mean absolute error",mean_absolute_error(Y_pred,y_test))
-proba_RF=pipe3.predict_proba(X_test)
-# print("Random Forest Tested")
+proba_RF=pipe3.predict_proba(X_test)            # predict on test data using trained model
+
 y_pred=[]
 value=10
 a=1
 c=8
+                                                                
+#probability score combination
 for i in range(len(X_test)):
     pro_score_0=a*proba_SVC[i][0]+c*proba_RF[i][0]
     pro_score_1=a*proba_SVC[i][1]+c*proba_RF[i][1]
     pro_score_2=a*proba_SVC[i][2]+c*proba_RF[i][2]
     pro_score_3=a*proba_SVC[i][3]+c*proba_RF[i][3]
 
-                    
+
     if(pro_score_0>=pro_score_1 and pro_score_0>=pro_score_2 and pro_score_0>=pro_score_3):
         y_pred.append(0)
     elif(pro_score_1>=pro_score_0 and pro_score_1>=pro_score_2 and pro_score_1>=pro_score_3):
@@ -124,7 +127,9 @@ for i in range(len(X_test)):
         y_pred.append(2)
     elif(pro_score_3>=pro_score_0 and pro_score_3>=pro_score_1 and pro_score_3>=pro_score_2):
         y_pred.append(3)
-                
+
+
+#convert y_pred to class label
 actual_Class=[]
 for i in range(len(X_test)):
     if y_pred[i]==0:
@@ -136,18 +141,18 @@ for i in range(len(X_test)):
     if y_pred[i]==3:
         actual_Class.append('UNK')
 
+
+#print count of labels
 print("Count of AFP",actual_Class.count("AFP"))
 print("Count of PC",actual_Class.count("PC"))
 print("Count of NTP",actual_Class.count("NTP"))
 print("Count of UNK",actual_Class.count("UNK"))
 
-# print(actual_Class.__len__())
-# Open the input_file in read mode and output_file in write mode
+
+#write in output csv file
 with open(output_path, 'w', newline='') as write_obj:
-    # Create a csv.reader object from the input file object
     # Create a csv.writer object from the output file object
     csv_writer = writer(write_obj)
-    # Read each row of the input csv file as list
     i=0
     j=0
     for row in range(no_rows):
